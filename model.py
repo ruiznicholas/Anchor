f77f3e7beef8bbb1092ec1274078c2ebbac73cb8
Nick R <ruiznicholas.32@gmail.com>
2020-08-04 16:58:26 -0400

    updated checkout to include pencil varients

--- model.py
+++ model.py
@@ -1,3 +1,6 @@
 def calc_charges(num):
-    final_num = float(num)*3.5
+    if int(num) % 100 == 0:
+        final_num = float(num)*3
+    else:
+        final_num = float(num)*3.5
     return final_num
