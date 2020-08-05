666d23e23cf09a94888ece866703d24bdf060f29
Nick R <ruiznicholas.32@gmail.com>
2020-08-05 10:53:53 -0400

    Changed price for pens. Minor astetic changes to checkout

--- model.py
+++ model.py
@@ -1,6 +1,6 @@
 def calc_charges(num):
     if int(num) % 100 == 0:
-        final_num = float(num)*3
+        final_num = float(num)*7.7
     else:
-        final_num = float(num)*3.5
+        final_num = float(num)*8.99
     return final_num
